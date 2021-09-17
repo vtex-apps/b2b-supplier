548b8146a99630639eb9d2fa70f74d61bc5efd10
Gabriel Souza <gabrie.ssouza@gmail.com>
2021-09-17 15:52:59 -0300

    fix(sign-up-url): point to join vtex

--- react/SignUpButton.tsx
+++ react/SignUpButton.tsx
@@ -4,29 +4,22 @@ import { FormattedMessage } from 'react-intl'
 
 import { isLoggedIn, useSession } from './session'
 
-const SIGN_UP_URL = 'https://b2bstore.myvtex.com/buyer'
+const SIGN_UP_URL = 'https://join.vtex.com/buyer'
 
 const SignUpButton = () => {
   const session = useSession()
 
   if (isLoggedIn(session)) return null
 
-  const handleNavigate = () => {
-    window.location.href = SIGN_UP_URL
-  }
-
   return (
     <div className="flex items-center">
-      <Button
-        variation="tertiary"
-        size="regular"
-        data-testid="signup-button"
-        onClick={handleNavigate}
-      >
-        <span className="f6 gray">
-          <FormattedMessage id="store/b2b-auth.signup" />
-        </span>
-      </Button>
+      <a href={SIGN_UP_URL} referrerPolicy="no-referrer-when-downgrade">
+        <Button variation="tertiary" size="regular" data-testid="signup-button">
+          <span className="f6 gray">
+            <FormattedMessage id="store/b2b-auth.signup" />
+          </span>
+        </Button>
+      </a>
     </div>
   )
 }
