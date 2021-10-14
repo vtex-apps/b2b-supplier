c77aecf068d52c1107c54523bb26c65e0596f6b0
Gabriel Souza <gabrie.ssouza@gmail.com>
2021-10-14 16:48:20 -0300

    feat(supplier): redirect when react /signup

--- react/SignUpButton.tsx
+++ react/SignUpButton.tsx
@@ -3,11 +3,21 @@ import { Button } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 import { useQuery } from 'react-apollo'
+import { useRuntime } from 'vtex.render-runtime'
 
 import SignUpButtonQuery from './graphql/SignUpButtonQuery.graphql'
 import { isLoggedIn, useSession } from './session'
 
-const SIGN_UP_URL = 'https://join.vtex.com/buyer'
+const SIGN_UP_URL = 'https://gsasouza--b2bstore.myvtex.com/buyer'
+const SIGN_UP_ROUTE = '/signup'
+
+const useSignUpStart = (handleRedirectSignup: () => void) => {
+  const { route } = useRuntime()
+
+  React.useEffect(() => {
+    if (route.canonicalPath === SIGN_UP_ROUTE) handleRedirectSignup()
+  }, [route.canonicalPath, handleRedirectSignup])
+}
 
 const SignUpButton = () => {
   const { data } = useQuery(SignUpButtonQuery, { ssr: false })
@@ -16,13 +26,17 @@ const SignUpButton = () => {
 
   const session = useSession()
 
-  if (isLoggedIn(session)) return null
+  const handleRedirectSignup = React.useCallback(() => {
+    if (!data?.supplierMeta) return
 
-  const handleNavigate = () => {
     const qs = new URLSearchParams({ supplier: data?.supplierMeta })
 
     window.location.href = `${SIGN_UP_URL}?${qs}`
-  }
+  }, [data?.supplierMeta])
+
+  useSignUpStart(handleRedirectSignup)
+
+  if (isLoggedIn(session)) return null
 
   return (
     <div className={`${handles.container ?? ''} flex items-center`}>
@@ -30,7 +44,7 @@ const SignUpButton = () => {
         variation="tertiary"
         size="regular"
         data-testid="signup-button"
-        onClick={handleNavigate}
+        onClick={handleRedirectSignup}
         className={handles.button}
       >
         <span className={`${handles.label ?? ''} f6 gray`}>
